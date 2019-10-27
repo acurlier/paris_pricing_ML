6b367c2eca076fa1a5540998dec3a4bd0c624eb7
acur28 <a.curlier@gmail.com>
2019-10-27 22:53:57 +0100

    onehotencoding of quartiers

--- PriceAnalysis.py
+++ PriceAnalysis.py
@@ -6,36 +6,49 @@ import matplotlib.pyplot as plt
 import numpy as np
 from pandas.io.json import json_normalize
 
-class PriceAnalysis():
 
-    def __init__(self,df_toworkwith):
+class PriceAnalysis:
 
-        self.raw_dataset = df_toworkwith
+    def __init__(self, df_toworkwith):
+        self.raw_dataset = df_toworkwith[0:100]
 
     def preprocess(self):
-        #keep only what's necessary
-        significant_features = ['Surface Carrez du 1er lot','Nombre pieces principales','Prix / m2','Quartier','lattitude','longitude']
-        self.raw_dataset = self.raw_dataset[significant_features]        
-        
+
+        # retrieve quartier name <-> quartier code dataset
+        adresse = 'https://opendata.paris.fr/explore/dataset/quartier_paris/download/?format=csv&timezone=Europe/Berlin&use_labels_for_header=true'
+        new_df = pd.read_csv(adresse, sep=";")
+        list_code = new_df['N_SQ_QU']
+        list_noms = new_df['L_QU']
+
+        # keep only what's necessary
+        significant_features = ['Surface Carrez du 1er lot', 'Nombre pieces principales', 'Prix / m2', 'Quartier',
+                                'lattitude', 'longitude']
+        self.raw_dataset = self.raw_dataset[significant_features]
+
         # remove outliers 
-        self.raw_dataset =  self.raw_dataset[ self.raw_dataset['Prix / m2']<30000]
-    
-        #separate features from target values
-        self.df_price_features = self.raw_dataset.drop(['Prix / m2'],axis=1)
-        self.df_price_target = self.raw_dataset['Prix / m2']
-        
-        #we will refer only to the administrative district for loaction
-        #self.df_price_features = self.df_price_features.drop(['lattitude','longitude'],axis=1)
-        quartier_list = self.df_price_features[['Quartier']]
-        self.df_price_features = self.df_price_features[['Surface Carrez du 1er lot','Nombre pieces principales','Prix / m2']]
+        self.raw_dataset = self.raw_dataset[self.raw_dataset['Prix / m2'] < 30000]
 
+        # separate features from target values
+        df_price_features = self.raw_dataset.drop(['Prix / m2'], axis=1)
+        df_price_target = self.raw_dataset['Prix / m2']
 
-        #perform hot one encoding on the district
+        # we will refer only to the administrative district for location
+        quartier_list = df_price_features[['Quartier']]
+        quartier_list.head()
+
+        # switch the code of the quartier for the name (otherwise, onehotencoder crashes bad)
+        df_price_features[['Quartier']] = df_price_features[['Quartier']].replace(list_code.to_list(),
+                                                                                  list_noms.to_list())
+
+        df_price_features = df_price_features[
+            ['Surface Carrez du 1er lot', 'Nombre pieces principales']]
+
+        # perform hot one encoding on the district
         cat_encoder = OneHotEncoder()
-        Quartier_1hot = cat_encoder.fit_transform(quartier_list)
+        quartier_1hot = cat_encoder.fit_transform(df_price_features[['Quartier']])
+        quartier_1hot_df = pd.DataFrame(quartier_1hot.toarray())
+        quartier_1hot_df.columns = cat_encoder.categories_
 
-        #concatenate the cont and onehotencoded dataframes
-        self.df_ready = pd.concat([self.df_price_features, Quartier_1hot], axis=1, join='inner')
-        self.df_ready.head()
-
-
+        # concatenate the cont and onehotencoded dataframes
+        df_ready = pd.concat([df_price_features, quartier_1hot_df], axis=1, join='inner')
+        print(df_ready.head(10))
--- main.py
+++ main.py
@@ -1,46 +1,50 @@
-
 import os
-#from os import listdir
-#from os.path import isfile, join
+# from os import listdir
+# from os.path import isfile, join
+import DatasetBuilder
 import PriceAnalysis
 
-my_analysis = PriceAnalysis.PriceAnalysis()
+my_dataset = DatasetBuilder.DatasetBuilder()
 
-#detect if a processed dataset is already in the folder
+# detect if a processed dataset is already in the folder
 if os.path.exists('df_price_clean') == True:
     print('existing processed data with geocoding detected')
     print('------')
 
-    my_analysis.df_price_clean = my_analysis.load_data('df_price_clean')
+    df_to_analyse = my_dataset.df_price_clean = my_dataset.load_data('df_price_clean')
 
-#otherwise it either download and process the data, or start computing the location 
+# otherwise it either download and process the data, or start computing the location
 else:
     if os.path.exists('df_price_paris') == True:
         print('existing processed data without geocoding detected')
         print('-------')
 
-        my_analysis.df_price_paris = my_analysis.load_data('df_price_paris')
-        my_analysis.generate_adresses()
-        my_analysis.find_coordinates()
-        my_analysis.get_geospatial_data()
-        my_analysis.save_data(my_analysis.df_price_clean,'df_price_clean')    
+        my_dataset.df_price_paris = my_dataset.load_data('df_price_paris')
+        my_dataset.generate_adresses()
+        my_dataset.find_coordinates()
+        my_dataset.get_geospatial_data()
+        my_dataset.save_data(my_dataset.df_price_clean, 'df_price_clean')
     else:
         if os.path.exists('df_price_original') == True:
             print('Original unprocessed dataset detected')
             print('------')
-            my_analysis.df_price = my_analysis.load_data('df_price_original')
+            my_dataset.df_price = my_dataset.load_data('df_price_original')
 
         else:
-            #start from scratch
+            # start from scratch
             print('No data already available')
             print('------')
-            my_analysis.get_price_data()
-            my_analysis.save_data(my_analysis.df_price,'df_price_original')
+            my_dataset.get_price_data()
+            my_dataset.save_data(my_dataset.df_price, 'df_price_original')
 
-        my_analysis.clean_dataset()
-        my_analysis.save_data(my_analysis.df_price_paris,'df_price_paris')
+        my_dataset.clean_dataset()
+        my_dataset.save_data(my_dataset.df_price_paris, 'df_price_paris')
 
-        my_analysis.generate_adresses()
-        my_analysis.find_coordinates()
-        my_analysis.get_geospatial_data()
-        my_analysis.save_data(my_analysis.df_price_clean,'df_price_clean')
+        my_dataset.generate_adresses()
+        my_dataset.find_coordinates()
+        my_dataset.get_geospatial_data()
+        my_dataset.save_data(my_dataset.df_price_clean, 'df_price_clean')
+
+print('Starting dataset analysis')
+my_analysis = PriceAnalysis.PriceAnalysis(df_to_analyse)
+my_analysis.preprocess()
